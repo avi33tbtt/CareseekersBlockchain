01c14335c4f17e3f48f03e9f2947a198cfca725b
Avinash kumar gupta <avi33tbtt@gmail.com>
2017-12-18 13:57:28 +0530

    Update Adoption.sol

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,1 +1,18 @@
+pragma solidity ^0.4.4;
 
+contract Adoption {
+address[16] public adopters;
+// Adopting a pet
+function adopt(uint petId) public returns (uint) {
+  require(petId >= 0 && petId <= 15);
+
+  adopters[petId] = msg.sender;
+
+  return petId;
+}
+// Retrieving the adopters
+function getAdopters() public returns (address[16]) {
+  return adopters;
+}
+
+}
