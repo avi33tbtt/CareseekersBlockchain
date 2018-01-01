074173adfa0aa69d59cd654d7646c6217da88308
Utkarsh shukla <utkarshthehellmaster@gmail.com>
2018-01-01 19:18:09 +0530

    Create generateNexthash.js
    Created the file for generating the contiguous hash functions.

---
+++ generateNextblock.js
@@ -0,0 +1,7 @@
+var generateNextBlock = (blockData) => {
+    var previousBlock = getLatestBlock();
+    var nextIndex = previousBlock.index + 1;
+    var nextTimestamp = new Date().getTime() / 1000;
+    var nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData);
+    return new Block(nextIndex, previousBlock.hash, nextTimestamp, blockData, nextHash);
+};
